4236d27cb02743b581c4e5651b39d25277463553
Ivan Pugach <ivan.pugach@spryker.com>
2019-02-04 23:44:35 +0200

    Update code after first round of AR

---
+++ src/Spryker/Client/ProductQuantityStorage/Validator/ProductQuantityItemValidatorInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\ProductQuantityStorage\Validator;
+
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\ItemValidationResponseTransfer;
+
+interface ProductQuantityItemValidatorInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     *
+     * @return \Generated\Shared\Transfer\ItemValidationResponseTransfer
+     */
+    public function validate(ItemTransfer $itemTransfer): ItemValidationResponseTransfer;
+}
